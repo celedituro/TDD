6ff1928e34ff000a54e7d5bdd714178cf19f302b
Celeste <celedituro@gmail.com>
2022-09-03 15:00:16 +0000

    Cambios en tests del controlador de entrada realizados

--- lucha/spec/controlador_de_entrada_spec.rb
+++ lucha/spec/controlador_de_entrada_spec.rb
@@ -3,21 +3,21 @@ require 'spec_helper'
 describe 'ControladorDeEntrada' do
   it '01-Recibe 2 Humano-con-arma y obtiene una lista con 2 Humano-con-arma' do
     luchadores = ControladorDeEntrada.new.obtener_luchadores([['humano', 'espada'], ['humano', 'espada']])
-    expect(luchadores).to eq [luchadores[0], luchadores[1]]
+    expect([luchadores[0].class, luchadores[1].class]).to eq [Humano, Humano]
   end
 
   it '02-Recibe Humano-con-arma y Humano-sin-arma y obtiene una lista con Humano-con-arma y Humano-sin-arma' do
     luchadores = ControladorDeEntrada.new.obtener_luchadores([['humano', 'espada'],['humano', 'mano']])
-    expect(luchadores).to eq [luchadores[0], luchadores[1]]
+    expect([luchadores[0].class, luchadores[1].class]).to eq [Humano, Humano]
   end
 
   it '03-Recibe Humano-sin-arma y Lobo-sin-arma y obtiene una lista con Humano-sin-arma y Lobo-sin-arma' do
     luchadores = ControladorDeEntrada.new.obtener_luchadores([['humano', 'espada'], ['lobo', 'mano']])
-    expect(luchadores).to eq [luchadores[0], luchadores[1]]
+    expect([luchadores[0].class, luchadores[1].class]).to eq [Humano, Lobo]
   end
 
-  it '04-Reciben 2 Lobo-con-arma y obtiene una lista con 2 Lobo-con-arma' do
+  it '04-Recibe 2 Lobo-con-arma y obtiene una lista con 2 Lobo-con-arma' do
     luchadores = ControladorDeEntrada.new.obtener_luchadores([['lobo', 'espada'], ['lobo', 'espada']])
-    expect(luchadores).to eq [luchadores[0], luchadores[1]]
+    expect([luchadores[0].class, luchadores[1].class]).to eq [Lobo, Lobo]
   end
 end
